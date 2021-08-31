702a2e053ca85afcb66d452cec93d90250b7ca62
Jordan Baker <jbb@scryent.com>
2021-08-31 13:56:33 -0600

    resize only on height change

--- src/ReadingPassage.vue
+++ src/ReadingPassage.vue
@@ -12,73 +12,104 @@
 </template>
 
 <script>
-import getLineHeight from 'line-height'
+import getLineHeight from "line-height";
 
 export default {
-  props: ['every'],
+  props: ["every"],
   data() {
     return {
       showLineNumbers: true,
       isMounted: false,
-      screenWidth: window.screen.width,
-    }
+      contentHeight: 0,
+    };
   },
   methods: {
     lines() {
-      const $content = this.$refs.content
-      if (!this.isMounted || !$content || !this.screenWidth) return
-      const lineHeight = getLineHeight($content)
-      const divHeight = $content.offsetHeight
-      const lineCount = divHeight / lineHeight
-      const byLine = this.every !== 'p'
-      const every = this.every ?? 1
-      const lines = Array.from({ length: lineCount })
-      if (byLine) {
-        const lineOffsets = Array.from(
-          { length: lineCount },
-          (_, i) => i + 1
-        ).filter((i) => !(i % every))
-        lineOffsets.forEach((offset) => (lines[offset - 1] = offset))
-      } else {
-        // by paragraph
+      // line calc requires mounted DOM
+      const $content = this.$refs.content;
+      if (!this.isMounted || !$content) return;
+
+      const every = this.every ?? 1;
+      const byLine = every !== "p";
+      const lineHeight = getLineHeight($content);
+      const lineCount = this.contentHeight / lineHeight;
+      // array of empty (undefined) line labels, length of total displayed line count
+      const lines = Array.from({ length: lineCount });
+
+      const linesEvery = (every) =>
+        Array.from({ length: lineCount }, (_, i) => i + 1).filter(
+          (i) => !(i % every)
+        );
+      const markOffset = (offset, value) => (lines[offset - 1] = value);
+
+      const paragraphLineNumbers = () =>
         // find all paragraphs
-        const lineOffsets = Array.from($content.getElementsByTagName('p'))
-          // offset from top of content
-          .map((el) => el.offsetTop - el.parentElement.offsetTop)
+        Array.from($content.getElementsByTagName("p"))
+          // get distance from top of content
+          .map(($p) => $p.offsetTop - $content.offsetTop)
           // calculate how many lines down it is
-          .map((p) => p / lineHeight + 1)
-        lineOffsets.forEach((offset, index) => (lines[offset - 1] = index + 1))
+          .map((p) => p / lineHeight + 1);
+
+      if (byLine) {
+        linesEvery(every).forEach((offset) => markOffset(offset, offset));
+      } else {
+        paragraphLineNumbers().forEach((offset, index) =>
+          markOffset(offset, index + 1)
+        );
       }
-      return lines
+
+      return lines;
     },
   },
   mounted() {
-    this.isMounted = true
-    let updateDimensions = () => {
-      this.showLineNumbers = true
-      this.$forceUpdate()
-    }
-    const hideNumbers = () => {
-      this.showLineNumbers = false
-    }
-    updateDimensions = debounceWithLeading(updateDimensions, hideNumbers, 300)
-    this.resizeObserver = new ResizeObserver(() => updateDimensions())
-    this.resizeObserver.observe(this.$refs.content)
-    return () => {
-      this.resizeObserver.disconnect()
-    }
-  },
-}
+    const $content = this.$refs.content;
+    this.contentHeight = $content.offsetHeight;
+    this.isMounted = true;
 
-function debounceWithLeading(debounceFunc, leadingFunc, timeout = 300) {
-  let timer
+    const updateDimensions = debounceWithLeading(
+      () => {
+        this.showLineNumbers = false;
+      },
+      () => {
+        this.contentHeight = $content.offsetHeight;
+        this.showLineNumbers = true;
+        window.requestAnimationFrame(() => this.$forceUpdate());
+      },
+      50
+    );
+
+    const observeContentHeight = () => {
+      const observeContentHeight = (entries) => {
+        for (let entry of entries) {
+          let newWidth = this.contentHeight;
+          if (entry.contentRect) {
+            newWidth = entry.contentRect.width;
+          } else {
+            newWidth = entry.contentBoxSize[0].blockSize;
+          }
+          if (newWidth !== this.contentHeight) updateDimensions();
+        }
+      };
+      const resizeObserver = new ResizeObserver(observeContentHeight);
+      resizeObserver.observe(this.$refs.content);
+      return () => {
+        resizeObserver.disconnect();
+      };
+    };
+
+    return observeContentHeight();
+  },
+};
+
+function debounceWithLeading(leadingFunc, debounceFunc, timeout = 300) {
+  let timer;
   return (...args) => {
-    clearTimeout(timer)
-    leadingFunc.apply(this, args)
+    clearTimeout(timer);
+    leadingFunc.apply(this, args);
     timer = setTimeout(() => {
-      debounceFunc.apply(this, args)
-    }, timeout)
-  }
+      debounceFunc.apply(this, args);
+    }, timeout);
+  };
 }
 </script>
 
