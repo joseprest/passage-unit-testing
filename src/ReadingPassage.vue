abf10ad83acd38ae9ceaecee12c67e47c24622e3
Jordan Brant Baker <jbb@scryent.com>
2021-08-31 14:46:38 -0600

    create line reference component

--- src/ReadingPassage.vue
+++ src/ReadingPassage.vue
@@ -12,104 +12,124 @@
 </template>
 
 <script>
-import getLineHeight from "line-height";
+import getLineHeight from 'line-height'
 
 export default {
-  props: ["every"],
+  props: ['every'],
+  inject: ['readingPassage'],
   data() {
     return {
       showLineNumbers: true,
       isMounted: false,
       contentHeight: 0,
-    };
+      lineHeight: 0,
+    }
   },
   methods: {
     lines() {
       // line calc requires mounted DOM
-      const $content = this.$refs.content;
-      if (!this.isMounted || !$content) return;
+      const $content = this.$refs.content
+      if (!this.isMounted || !$content) return
 
-      const every = this.every ?? 1;
-      const byLine = every !== "p";
-      const lineHeight = getLineHeight($content);
-      const lineCount = this.contentHeight / lineHeight;
+      const every = this.every ?? 1
+      const byLine = every !== 'p'
+      const lineCount = this.contentHeight / this.lineHeight
       // array of empty (undefined) line labels, length of total displayed line count
-      const lines = Array.from({ length: lineCount });
+      const lines = Array.from({ length: lineCount })
 
       const linesEvery = (every) =>
         Array.from({ length: lineCount }, (_, i) => i + 1).filter(
           (i) => !(i % every)
-        );
-      const markOffset = (offset, value) => (lines[offset - 1] = value);
+        )
+      const markOffset = (offset, value) => (lines[offset - 1] = value)
 
       const paragraphLineNumbers = () =>
         // find all paragraphs
-        Array.from($content.getElementsByTagName("p"))
+        Array.from($content.getElementsByTagName('p'))
           // get distance from top of content
           .map(($p) => $p.offsetTop - $content.offsetTop)
           // calculate how many lines down it is
-          .map((p) => p / lineHeight + 1);
+          .map((p) => p / this.lineHeight + 1)
 
       if (byLine) {
-        linesEvery(every).forEach((offset) => markOffset(offset, offset));
+        linesEvery(every).forEach((offset) => markOffset(offset, offset))
       } else {
         paragraphLineNumbers().forEach((offset, index) =>
           markOffset(offset, index + 1)
-        );
+        )
       }
 
-      return lines;
+      return lines
     },
   },
   mounted() {
-    const $content = this.$refs.content;
-    this.contentHeight = $content.offsetHeight;
-    this.isMounted = true;
+    const $content = this.$refs.content
+    this.contentHeight = $content.offsetHeight
+    this.isMounted = true
+    this.lineHeight = getLineHeight($content)
+
+    const updateLineRefs = () => {
+      const $refs = $content.querySelectorAll('a[id]')
+      $refs.forEach(($ref) => {
+        const refOffset = $ref.offsetTop
+        const contentOffset = $content.offsetTop
+        const line = (refOffset - contentOffset) / this.lineHeight + 1
+        this.readingPassage.update($ref.getAttribute('id'), Math.round(line))
+      })
+    }
+
+    const updateView = () => {
+      this.lineHeight = getLineHeight($content)
+      this.contentHeight = $content.offsetHeight
+      this.showLineNumbers = true
+      updateLineRefs()
+    }
 
     const updateDimensions = debounceWithLeading(
       () => {
-        this.showLineNumbers = false;
+        this.showLineNumbers = false
       },
       () => {
-        this.contentHeight = $content.offsetHeight;
-        this.showLineNumbers = true;
-        window.requestAnimationFrame(() => this.$forceUpdate());
+        window.requestAnimationFrame(() => {
+          updateView()
+          this.$forceUpdate()
+        })
       },
       50
-    );
+    )
 
     const observeContentHeight = () => {
       const observeContentHeight = (entries) => {
         for (let entry of entries) {
-          let newWidth = this.contentHeight;
+          let newWidth = this.contentHeight
           if (entry.contentRect) {
-            newWidth = entry.contentRect.width;
+            newWidth = entry.contentRect.width
           } else {
-            newWidth = entry.contentBoxSize[0].blockSize;
+            newWidth = entry.contentBoxSize[0].blockSize
           }
-          if (newWidth !== this.contentHeight) updateDimensions();
+          if (newWidth !== this.contentHeight) updateDimensions()
         }
-      };
-      const resizeObserver = new ResizeObserver(observeContentHeight);
-      resizeObserver.observe(this.$refs.content);
+      }
+      const resizeObserver = new ResizeObserver(observeContentHeight)
+      resizeObserver.observe(this.$refs.content)
       return () => {
-        resizeObserver.disconnect();
-      };
-    };
+        resizeObserver.disconnect()
+      }
+    }
 
-    return observeContentHeight();
+    return observeContentHeight()
   },
-};
+}
 
 function debounceWithLeading(leadingFunc, debounceFunc, timeout = 300) {
-  let timer;
+  let timer
   return (...args) => {
-    clearTimeout(timer);
-    leadingFunc.apply(this, args);
+    clearTimeout(timer)
+    leadingFunc.apply(this, args)
     timer = setTimeout(() => {
-      debounceFunc.apply(this, args);
-    }, timeout);
-  };
+      debounceFunc.apply(this, args)
+    }, timeout)
+  }
 }
 </script>
 
---
+++ src/ReadingPassageReference.vue
@@ -0,0 +1,18 @@
+<template>
+  <span>{{ line }}</span>
+</template>
+
+<script>
+export default {
+  props: ['snippet-id'],
+  inject: ['readingPassage'],
+  computed: {
+    line() {
+      const line = this.readingPassage.state[this.$props.snippetId]
+      return line
+    },
+  },
+}
+</script>
+
+<style lang="scss"></style>
